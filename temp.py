3272272b4aa9feda80274c525fdb0aef3ca828b9
mayank662 <mayankgoyal662@gmail.com>
2021-05-04 20:00:49 +0530

    Created main logic

---
+++ temp.py
@@ -0,0 +1,51 @@
+from tweepy import *
+import tweepy
+import re 
+import string
+import preprocessor as p
+
+# Twitter credentials
+consumer_key = 'z18WrER64TXmSgHOLijYhu8oj'
+consumer_secret = 'TITG51MsArRqRXQ49g5U8nUDI4yb8M9oPhBM0qbTiM7AodYdyp'
+access_key = '1354434577096445953-QMdZip0jp4DB37Ma6QHx1r24IGUES8'
+access_secret = '4QWNI0719wojkDAOV3BPaZEkXY46q08CphcaVG0EON4jp'
+
+# Pass your twitter credentials to tweepy via its OAuthHandler
+ 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+ 
+api = tweepy.API(auth,wait_on_rate_limit=True)
+ 
+search_words = "covid+helpline"      # enter your words
+new_search = search_words + " -filter:retweets"
+
+obj = tweepy.Cursor(api.search, q=new_search, count=100, lang='en', since_id=0).items(100)
+
+
+date = []
+content = []
+name = []
+location = []
+link = []
+for tweet in obj:
+	date.append(tweet.created_at)
+	content.append(tweet.text.encode('utf-8'))
+	name.append(tweet.user.screen_name.encode('utf-8'))
+	location.append(tweet.user.location.encode('utf-8'))
+	link.append(re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tweet.text))
+	
+print(date)
+print(len(date))
+print()
+print(content)
+print(len(content))
+print()
+print(name)
+print(len(name))
+print()
+print(location)
+print(len(location))
+print()
+print(link)
+print(len(link))
