7dc1552c43480fe32f8965f656bb3310b88c0f3e
mayank662 <mayankgoyal662@gmail.com>
2021-05-05 00:59:33 +0530

    Changed File

--- covelp/settings.py
+++ covelp/settings.py
@@ -24,11 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
 SECRET_KEY = config.secret_key
-=======
-SECRET_KEY = ''
->>>>>>> 05faf74699d1a20482febb2f467543ce189ac2e2
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
